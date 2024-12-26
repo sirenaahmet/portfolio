9a12a989f309699728ee585d94faa6d68d412086
Sirena <sirena.ahmet@outlook.com>
2024-12-26 10:33:30 +0100

    portfolio project

---
+++ script.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const filterButtons = document.querySelectorAll(".filter_btn"); 
+  const projects = document.querySelectorAll(".portfolio_body .project_coded, .portfolio_body .project_designed"); 
+
+  filterButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+      const filter = button.dataset.filter; 
+
+      projects.forEach((project) => {
+        if (filter === "all") {
+          project.style.display = "block"; 
+        } else if (filter === "coded" && project.classList.contains("project_coded")) {
+          project.style.display = "block"; 
+        } else if (filter === "designed" && project.classList.contains("project_designed")) {
+          project.style.display = "block"; 
+        } else {
+          project.style.display = "none"; 
+        }
+      });
+
+      /* filterButtons.forEach((btn) => btn.classList.remove("active"));
+      button.classList.add("active"); */
+    });
+  });
+});
+
+
